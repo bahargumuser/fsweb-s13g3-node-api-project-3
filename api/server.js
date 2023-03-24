7e86e29647bb2cf5e97c551d92ec549dec371347
bahargumuser <121288876+bahargumuser@users.noreply.github.com>
2023-03-24 16:39:39 +0300

    task completed

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/users-router");
+const { logger } = require("./middleware/middleware");
 
 const server = express();
+server.use(express.json());
 
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
 
 // global ara yazılımlar ve kullanıcı routelarının buraya bağlanması gerekir
+server.use("/api/users", userRouter);
+server.use(logger); //server'a yapılan her istekte logger çalışır
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
 });
 
